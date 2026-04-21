ea13f3a2578924304c99671c108a75ab82404338
Andrew Cho <andrewncho15@gmail.com>
2026-04-21 15:41:03 -0700

    attempt #1

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import statsmodels.formula.api as smf
+
+turkeydf = pd.read_csv("/Users/andrewcho/Downloads/01_turkey_animal_data.csv");
+print(turkeydf.dtypes)
+
+model = smf.ols("ADG ~ C(Diet)", data=turkeydf).fit()
+
+print(model.summary())
